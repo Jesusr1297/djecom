135ebb88a62acab8b87fe92ec3a3629a63bc1619
jesusr1297 <jesusr1297@outlook.com>
2022-09-24 14:38:39 -0500

    cart items now showing in navbar

--- store/views.py
+++ store/views.py
@@ -7,8 +7,18 @@ from . import models
 
 # Create your views here.
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = models.Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cart_items = order['get_cart_items']
+
     products = models.Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cart_items}
     return render(request=request, template_name='store/store.html', context=context)
 
 
